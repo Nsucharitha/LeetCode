183d93313aa44a37dd7cbcbf9e213a1a6dcb4c47
Sucharitha Nadendla <83368012+Nsucharitha@users.noreply.github.com>
2024-09-02 12:47:29 -0400

    Time: 27 ms (94.26%) | Memory: 16.4 MB (72.08%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,11 +1,13 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        L = []
-        for i, j in zip(word1, word2):
-            L.append(i + j)
-        L.append(word1[len(word2):])
-        L.append(word2[len(word1):])
-        return "".join(L)
+        L = ""
+        for i in range(min(len(word1),len(word2))):
+            L = L + word1[i] + word2[i]
+        if len(word1) < len(word2):
+            L = L + word2[len(word1):]
+        else:
+            L = L + word1[len(word2):]
+        return L
         
 
         
