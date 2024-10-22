3149a31882b5db788e475cb0df66321341baa4f9
Sucharitha Nadendla <83368012+Nsucharitha@users.noreply.github.com>
2024-10-22 16:05:27 -0400

    Time: 1667 ms (27.77%) | Memory: 17.1 MB (99.80%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,4 +6,18 @@ class Solution:
                 if target == nums[i]+nums[j]:
                     return [i,j]
 
-        
+'''
+Solution using Hashmap gives Time complexity as O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        h = {}
+        for i in range(n):
+            h[nums[i]] = i
+        for j in range(n):
+            y = target - nums[i]
+            if y in h and h[y]!=i:
+                return [i,h[y]]
+
+'''
