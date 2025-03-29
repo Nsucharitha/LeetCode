a73335696bdf10e73064da3060af5c971b585913
Sucharitha Nadendla <83368012+Nsucharitha@users.noreply.github.com>
2025-03-29 16:34:28 -0400

    Time: 3 ms (43.62%) | Memory: 23.1 MB (16.34%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -7,4 +7,29 @@ class Solution:
         while l<r:
             s[l], s[r] = s[r], s[l]
             l, r = l+1, r-1
-        
+
+
+"""
+Using Stack, 
+time : O(n)
+Space : O(n)
+
+Stack = []
+for c in s:
+    stack.append(c)
+i =0
+while stack:
+    s[i] = stack.pop()
+    i+=1
+
+Using Recursion
+time : O(n)
+Space : O(n)
+
+def reverse(l,r):
+    if l<r:
+        s[l],s[r] = s[r],s[l]
+        reverse(l+1, r-1)
+reverse(0,len(s)-1)
+
+"""
