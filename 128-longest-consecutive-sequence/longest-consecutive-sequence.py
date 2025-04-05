83df2dab363cd00320576b60b58f5e0e63c0c641
Sucharitha Nadendla <83368012+Nsucharitha@users.noreply.github.com>
2025-04-05 13:41:13 -0400

    Time: 45 ms (81.08%) | Memory: 34.2 MB (53.04%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,10 +4,8 @@ class Solution:
         longest = 0
         for n in numSet:
             if (n-1) not in numSet:
-                next_num = n+1
-                length = 1
-                while next_num in numSet:
+                length = 0
+                while (n+length) in numSet:
                     length += 1
-                    next_num += 1
                 longest = max(length, longest)
         return longest        
